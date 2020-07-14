e815183b38579ef95fcc5340fa20a8a96697da31
guhan29 <mguhan439@gmail.com>
2020-07-14 22:24:20 +0530

    created lists in index

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index() {
-    	return view('todos.index');
+    	$todos = Todo::all();
+    	// dd($todos);
+    	// $todos = Todo::orderBy('id', asc)->get();
+    	return view('todos.index', ['todos' => $todos]);
     }
 
     public function edit() {
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
 <h1>Hello index</h1>
+	<ul>
+		@foreach($todos as $todo)
+			<li>
+				@if($todo->is_completed)
+					<p><del>{{ $todo->todo_text }}</del></p>
+				@else
+					<p>{{ $todo->todo_text }}</p>
+				@endif
+				<div class="buttons">
+					<a class="btn btn-success" href="#">Toggle</a>
+					<a class="btn btn-info" href="#">Edit</a>
+					<a class="btn btn-danger" href="#">Delete</a>
+				</div>
+			</li>
+		@endforeach
+	</ul>
 @endsection
